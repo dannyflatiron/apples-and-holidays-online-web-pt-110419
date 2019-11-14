c207fb3021e8b3cf31dead86f9e92ba289bbb604
Daniel Reina <dannyflatiron@gmail.com>
2019-11-14 22:58:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,6 +43,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # remember to return the updated hash
   holiday_hash[:fall][:thanksgiving]<<holiday_name
   holiday_hash
+  binding.pry
 
 end
 
