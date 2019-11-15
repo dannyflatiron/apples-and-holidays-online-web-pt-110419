6aa2c0a2a60f33f32eb156cd4d21748b8fed7753
Daniel Reina <dannyflatiron@gmail.com>
2019-11-15 00:31:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,8 +51,8 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   # holiday_hash[:winter][:christmas] << "Party Hats"
   holiday_hash[:winter].map do |key, value|
-    value.flatten
-  end
+    value
+  end.flatten
   # binding.pry
 end
 
